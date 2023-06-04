5ff96f6db142eb20a883222bb469c141ce02d6e7
fniad <artkamproject@gmail.com>
2023-06-04 16:49:42 +0300

    hw5

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,24 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node=None):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
-    def insert_beginning(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
-
-    def insert_at_end(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+    def __init__(self):
+        """Конструктор класса Queue"""
+        self.tail = None
+        self.head = None
+        self.length = 0
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -26,4 +32,41 @@ class LinkedList:
             node = node.next_node
 
         ll_string += ' None'
-        return ll_string
+        return ll_string[1:]
+
+    def __repr__(self):
+        """Представление списка в виде строки для разработки"""
+        node = self.head
+        values = []
+        while node:
+            values.append(str(node.data))
+            node = node.next_node
+        return " -> ".join(values)
+
+    def __len__(self):
+        """Возвращает длину списка"""
+        return self.length
+
+    def insert_beginning(self, data: dict) -> None:
+        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
+        self.length += 1
+
+    def insert_at_end(self, data: dict) -> None:
+        """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+        self.length += 1
+
+
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,71 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import Node, LinkedList
+
+
+class TestNode(unittest.TestCase):
+    """ Тестирование функции Node """
+
+    def setUp(self):
+        """ Тестовые данные для теста: заданные два объекта класса Node """
+        self.node_first = Node(5, None)
+        self.node_second = Node('a', 5)
+
+    def tearDown(self):
+        pass
+
+    def test_Node(self):
+        """ Тест атрибутов класса Node """
+        self.assertEqual(self.node_first.data, 5)  # проверяем как считывается атрибут data
+        self.assertEqual(self.node_second.data, 'a')
+        self.assertEqual(self.node_second.next_node, 5)  # проверяем как считывается атрибут next_node
+        self.assertEqual(self.node_first.next_node, None)
+
+
+class TestLinkedList(unittest.TestCase):
+    """
+
+    """
+    def setUp(self):
+        """ Тестовые данные для теста: заданные два объекта класса LinkedList """
+        self.ll = LinkedList()
+        # Добавляем данные
+        self.ll.insert_beginning({'id': 1})
+        self.ll.insert_at_end({'id': 2})
+        self.ll.insert_at_end({'id': 3})
+        self.ll.insert_beginning({'id': 0})
+
+    def tearDown(self):
+        pass
+
+    def test_str(self):
+        """Проверяем вывод строки пользователю об элементах односвязного списка"""
+        self.assertEqual(str(LinkedList()), 'None')  # если список пуст
+        # если в список добавлено 4 объекта
+        self.assertEqual(str(self.ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+    def test_repr(self):
+        """Представление списка в виде строки для разработки"""
+        self.assertEqual(LinkedList().__repr__(), '')
+        self.assertEqual(self.ll.__repr__(), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3}")
+
+    def test_len(self):
+        """Возвращает длину списка"""
+        self.assertEqual(self.ll.__len__(), 4)  # в списке 4 словаря
+
+    def test_insert_beginning(self):
+        """Добавление элемента вперёд списка"""
+        self.ll.insert_beginning({'id': -1})
+        self.assertEqual(self.ll.__repr__(), "{'id': -1} -> {'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3}")
+
+        # проверка на пустом списке
+        ll2 = LinkedList()
+        ll2.insert_beginning({'id': 0})
+        self.assertEqual(ll2.head.data, {'id': 0})
+        self.assertEqual(ll2.tail.data, {'id': 0})
+        self.assertEqual(ll2.__repr__(), "{'id': 0}")
+
+    def test_insert_at_end(self):
+        """Добавление элемента в конец списка"""
+        self.ll.insert_at_end({'id': 4})
+        self.assertEqual(self.ll.__repr__(), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> {'id': 4}")
